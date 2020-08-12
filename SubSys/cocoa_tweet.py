b29ec453d2c829d1b9487c1d2e69f7467c4f2358
unagi <30.unagi@gmail.com>
2020-08-13 02:24:48 +0900

    Sheetデータの閲覧リンクを仮追加

--- SubSys/cocoa_tweet.py
+++ SubSys/cocoa_tweet.py
@@ -80,6 +80,9 @@ def tweet():
     text10 = "Google Sheetにてグラフ公開\n"
     text11 = "https://t.co/doJh6Y0Uzm?amp=1\n\n"
 
+    text12 = "Sheetデータを閲覧用で公開"
+    text13 = "https://docs.google.com/spreadsheets/d/1mq_yfYhArPPoL_n3zNZSoL_dMaxGLMmBkVnM3gcJGgM/edit?usp=sharing"
+
     # ツイート本文生成
     twitter_first = text1 + text2 + text3 + text4 + text5 + text6   + text7
     twitter_second = text1 + text9 + text10 + text11   + text7
