2a85baed2ce08c0aea3be5c64cdf02b52bab2264
Unagi2 <30.unagi@gmail.com>
2020-08-20 14:54:51 +0900

    ツイート文修正

--- sub_sys/cocoa_tweet.py
+++ sub_sys/cocoa_tweet.py
@@ -71,7 +71,7 @@ def tweet():
     text7 = "#COCOA #接触確認アプリ"
 
     text9 = "更新日毎の増分変化についてのグラフ\n\n"
-    text10 = "Google Data Portalにてレポートを公開しています。\n"
+    text10 = "Google Data Portalにてレポート公開中\n"
     text11 = "https://datastudio.google.com/u/0/reporting/f9081247-b6d3-48b8-a6e2-82d3b0b018ce/page/EczbB\n\n"
 
     text12 = "Sheetデータを閲覧用で公開"
