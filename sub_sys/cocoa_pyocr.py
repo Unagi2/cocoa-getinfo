cbfd2ba8f73a203a21b4fab6288ee13d042b6394
Unagi2 <30.unagi@gmail.com>
2021-02-26 19:58:04 +0900

    "Data update"

--- sub_sys/cocoa_pyocr.py
+++ sub_sys/cocoa_pyocr.py
@@ -52,7 +52,7 @@ def py_ocr():
     """
     img_rgb = img.convert("RGB")
     pixels = img_rgb.load()            
-    c_max = 130 #169
+    c_max = 180 #169
     for j in range(img_rgb.size[1]):
         for i in range(img_rgb.size[0]):
             if (pixels[i, j][0] > c_max or pixels[i, j][1] > c_max or
